a4a03b5c375aec20822570cb178efdac22f1d3d5
Wesley Silva <wesleyysilva@yahoo.com.br>
2022-07-19 12:26:20 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
+require("express-async-errors");
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const user = users.find((user) => user.name === name);
+
+  if (user) {
+    response.status(500).json({
+      status: "error",
+      message: `The user exists`,
+    });
+  }
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+  const { name, username } = request.body;
+
+  users.push(
+    {
+      id: uuid(),
+      name,
+      username
+    }
+  );
+
+  return response.status(201).json(users);
+});
+
+app.get('/users', (request, response) => {
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
